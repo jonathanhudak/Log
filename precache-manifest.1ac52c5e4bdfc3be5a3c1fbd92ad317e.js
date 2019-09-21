f85c9cf16b5a833ee1e3da615e6e0c227def8127
Jonathan Hudak <hudak.jonathan@gmail.com>
2019-09-21 14:20:48 -0700

    Updates

---
+++ precache-manifest.1ac52c5e4bdfc3be5a3c1fbd92ad317e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b23ac98fb8d172b466d2b09e7ec80de",
+    "url": "/Log/index.html"
+  },
+  {
+    "revision": "a065f6afaec507a64add",
+    "url": "/Log/static/js/2.bb7772eb.chunk.js"
+  },
+  {
+    "revision": "b021190508a36103534f",
+    "url": "/Log/static/js/main.b036556b.chunk.js"
+  },
+  {
+    "revision": "3d406c51a3aeced622f7",
+    "url": "/Log/static/js/runtime~main.62d9c59f.js"
+  }
+]);
